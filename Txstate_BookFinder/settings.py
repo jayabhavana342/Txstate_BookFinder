d4ac80177fe322e76e9f5d55b9cccf01646b54a0
Bhavana Kommuru <jayanagabhavanakommuru@gmail.com>
2018-03-23 18:50:37 -0500

    created and registered model user

--- Txstate_BookFinder/settings.py
+++ Txstate_BookFinder/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'booksgallery',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom Django auth settings
+
+AUTH_USER_MODEL = 'booksgallery.User'
+
+LOGIN_URL = 'login'
+
+LOGOUT_URL = 'logout'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
